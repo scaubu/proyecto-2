1d14dd6c41e5233421810555cf59f617540ea6cf
scaubu <s.caubu@hotmail.com>
2023-05-26 23:49:38 -0300

    commit test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {GiHamburgerMenu} from "react-icons/gi";
+import {MdOurlineRestaurantMenu} from "react-icons/md";
 
+import images from "../../constants/image";
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'> 
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt="app logo" />
+    </div>
+  </nav>
 );
 
 export default Navbar;
